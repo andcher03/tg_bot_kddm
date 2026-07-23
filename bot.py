e5db53e609e89206c1aca2f9f0baf255f4a36757
andcher03 <andcher03@gmail.com>
2026-07-23 11:14:15 +0300

    add config and changed bot

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+async def main():
+    print("Бот успешно запущен!")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
